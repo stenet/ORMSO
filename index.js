442894567919e893a3f6dec3083344985d17a39a
stenet <stefan.heim@hotmail.com>
2015-08-16 00:34:45 +0200

    v0.0.5

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+var dl = require("./lib/DataLayer");
+var dc = require("./lib/DataContext");
+var sc = require("./lib/SyncContext");
+var pc = require("./lib/PublishContext");
 //# sourceMappingURL=index.js.map
